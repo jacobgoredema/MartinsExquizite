f625d4c57ff700f5cd35b66508c623dda65bead3
Jacob <jacobgoredema@gmail.com>
2019-09-13 16:00:02 +0200

    Initial Commit

---
+++ MartinsExquizite.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MartinsExquizite.Web.MvcApplication" Language="C#" %>
---
+++ MartinsExquizite.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MartinsExquizite.Web.Startup))]
+namespace MartinsExquizite.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
