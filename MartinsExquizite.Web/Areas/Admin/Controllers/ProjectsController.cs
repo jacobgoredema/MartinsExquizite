1847a1330148cf4cec9998d081235ed6cd815ee7
Jacob <jacobgoredema@gmail.com>
2019-10-01 15:44:55 +0200

    admin layout completed

---
+++ MartinsExquizite.Web/Areas/Admin/Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MartinsExquizite.Web.Areas.Admin.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Admin/Projects
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
