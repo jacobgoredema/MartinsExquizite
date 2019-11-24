287b508fca45660b55ba08115c612c1e94d81bb1
Jay <Jay@DEV-PC>
2019-11-24 21:45:28 +0200

    working on category Create Page

--- MartinsExquizite.Web/Areas/Admin/AdminAreaRegistration.cs
+++ MartinsExquizite.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -2,42 +2,129 @@
 
 namespace MartinsExquizite.Web.Areas.Admin
 {
-    public class AdminAreaRegistration : AreaRegistration 
+    public class AdminAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Admin";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "Admin",
                 "Admin",
-                new { controller="Admin", action = "Index", id = UrlParameter.Optional }
+                new
+                {
+                    controller = "Admin",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
 
-
-
+            context.MapRoute(
+                "UnAuthorized",
+                "UnAuthorized",
+                new { controller = "Base", action = "UnAuthorized" }
+            );
 
             context.MapRoute(
                 "EntityList",
-                "Dashboard/{controller}/",
+                "Admin/{controller}/",
                 new { action = "Index" }
-                );
+            );
 
+            context.MapRoute(
+                "EntityCreate",
+                "Admin/{controller}/Create/",
+                new { action = "Action" }
+            );
 
+            context.MapRoute(
+                "EntityEdit",
+                "Admin/{controller}/Edit/{id}",
+                new { action = "Action" }
+            );
+
+            context.MapRoute(
+                "EntityEditWithoutID",
+                "Admin/{controller}/Edit/",
+                new { action = "Action" }
+            );
+
+            context.MapRoute(
+                "EntityDetails",
+                "Dashboard/{controller}/Details/{id}",
+                new { action = "Details" }
+            );
 
             context.MapRoute(
                 "EntityDelete",
-                "Dashboard/{controller}/Delete",
+                "Admin/{controller}/Delete/",
                 new { action = "Delete" }
-                );
+            );
 
+            context.MapRoute(
+                name: "UserDetails",
+                url: "Admin/Users/UserDetails/{userID}",
+                defaults: new { controller = "Users", action = "UserDetails" }
+            );
 
+            context.MapRoute(
+                name: "UserRoles",
+                url: "Admin/Users/UserRoles/{userID}",
+                defaults: new { controller = "Roles", action = "UserRoles" }
+            );
+
+            context.MapRoute(
+                name: "RoleDetails",
+                url: "Admin/Roles/RoleDetails/{roleID}",
+                defaults: new { controller = "Roles", action = "RoleDetails" }
+            );
+
+            context.MapRoute(
+                name: "RoleUsers",
+                url: "Dashboard/Roles/RoleUsers/{roleID}",
+                defaults: new { controller = "Roles", action = "RoleUsers" }
+            );
+
+            context.MapRoute(
+                name: "AssignUserRole",
+                url: "Admin/Roles/UserRole/{userID}/Assign/",
+                defaults: new { controller = "Roles", action = "AssignUserRole" }
+            );
+
+            context.MapRoute(
+                name: "DeleteUserRole",
+                url: "Admin/Roles/UserRole/{userID}/Delete/",
+                defaults: new { controller = "Roles", action = "DeleteUserRole" }
+            );
+
+            context.MapRoute(
+                name: "OrdersByEmail",
+                url: "Admin/Orders/OrdersByEmail",
+                defaults: new { controller = "Orders", action = "OrdersByEmail" }
+            );
+
+            context.MapRoute(
+                name: "UpdateOrderStatus",
+                url: "Admin/Orders/{orderID}/Update-Status/",
+                defaults: new { controller = "Orders", action = "UpdateStatus" }
+            );
+
+            context.MapRoute(
+                "Admin_Default",
+                "Admin/{controller}/{action}/{id}",
+                new
+                {
+                    controller = "Dashboard",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
         }
     }
 }
